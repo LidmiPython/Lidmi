09b8b817ba6face0bd1b274416a6013d471ff0b7
Josesofdess <67865208+Josesofdess@users.noreply.github.com>
2020-10-31 13:14:52 +0200

    Update send_message.py

--- pyrogram/send_message.py
+++ pyrogram/send_message.py
@@ -5,7 +5,7 @@ app = Client("session_name",
              api_hash="******",
              lang_code="ru")
 
-@app.on_message(message_text_filter(f"test2435346456453464565675675465476567567"))
+@app.on_message()
 async def test_send_message(client, message):
   await app.send_message(message.chat.id, f'''тестовое сообщение''')#отправить сообщение в чат
   await app.send_message(message.from_user.id, f'''тестовое сообщение''')#отправить сообщение юзеру
