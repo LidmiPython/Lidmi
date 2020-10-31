15fd30ea27eab282853d6d9965611ec100eb5849
Josesofdess <67865208+Josesofdess@users.noreply.github.com>
2020-10-31 13:33:03 +0200

    Update send_voice.py

--- pyrogram/send_voice.py
+++ pyrogram/send_voice.py
@@ -8,8 +8,14 @@ app = Client("session_name",
 @app.on_message()
 async def test_send_voice(client, message):
   voice = '1DirectedbyRobertWeide.ogg'
-  await app.send_voice(message.chat.id, voice, reply_to_message_id=message.reply_to_message.message_id)# отправить в чат и ответить на сообщение на которое вы ответили
   await app.send_voice(message.chat.id, voice)# отправить войс в чат
-
-  await app.send_voice(message.from_user.id, voice, reply_to_message_id=message.reply_to_message.message_id)# отправить войс юзеру и ответить на сообщение на которое вы ответили
   await app.send_voice(message.from_user.id, voice)# отправить войс юзеру
+
+  await app.send_voice(message.chat.id, voice, reply_to_message_id=message.reply_to_message.message_id)# отправить в чат и Ответитив на сообщение, используя его идентификатор
+  await app.send_voice(message.from_user.id, voice, reply_to_message_id=message.reply_to_message.message_id)# отправить войс юзеру и Ответитив на сообщение, используя его идентификатор
+  
+  await app.send_voice(message.chat.id, voice, caption="voice note")# отправить войс в чат и Добавть подпись к голосовому сообщению
+  await app.send_voice(message.from_user.id, voice, caption="voice note")# отправить войс юзеру и Добавть подпись к голосовому сообщению
+
+  await app.send_voice(message.chat.id, voice,, reply_to_message_id=message.reply_to_message.message_id, caption="voice note")# отправить войс в чат и Ответитив на сообщение, используя его идентификатор и Добавть подпись к голосовому сообщению
+  await app.send_voice(message.from_user.id, voice,, reply_to_message_id=message.reply_to_message.message_id, caption="voice note")# отправить войс юзеру и Ответитив на сообщение, используя его идентификатор и Добавть подпись к голосовому сообщению
