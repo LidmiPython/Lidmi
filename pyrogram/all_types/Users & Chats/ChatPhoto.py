331527a7ce6a9593ebd1c6da71ea69e92acba382
Josesofdess <67865208+Josesofdess@users.noreply.github.com>
2020-11-06 11:49:21 +0200

    Create ChatPhoto.py

---
+++ pyrogram/all_types/Users & Chats/ChatPhoto.py
@@ -0,0 +1,5 @@
+#class pyrogram.types.ChatPhoto
+#https://docs.pyrogram.org/api/types/ChatPhoto#pyrogram.types.ChatPhoto
+
+message.chat.photo.small_file_id        # ( str) - Идентификатор файла маленькой (160x160) фотографии чата. Этот file_id можно использовать только для загрузки фотографий и только до тех пор, пока фотография не будет изменена.
+message.chat.photo.big_file_id          # ( str) - Идентификатор файла большой (640x640) фотографии чата. Этот file_id можно использовать только для загрузки фотографий и только до тех пор, пока фотография не будет изменена.
