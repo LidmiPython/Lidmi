3697bfee4d772879e2c53f401242a9c22227707a
Josesofdess <67865208+Josesofdess@users.noreply.github.com>
2020-11-09 02:58:45 +0200

    Update GetFullChannel.py

--- pyrogram/Raw Functions/Channels/GetFullChannel.py
+++ pyrogram/Raw Functions/Channels/GetFullChannel.py
@@ -1,8 +1,8 @@
 from pyrogram.raw.functions.channels import GetFullChannel
 
 #Получить полную информацию о канале
-@app.on_message(message_text_filter(f"123123123"))
+@app.on_message()
 async def channels_GetFullChannel(client, message):
 	msg = await app.send(GetFullChannel(
-		channel=await app.resolve_peer(-1001322182991)))
+		channel=await app.resolve_peer(-1001158541073)))
 	print(msg)
