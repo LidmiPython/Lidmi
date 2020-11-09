792f09258264f504b11761f512cb251716dd56d0
Josesofdess <67865208+Josesofdess@users.noreply.github.com>
2020-11-09 06:13:37 +0200

    Update not tested.py

--- temporary directory/not tested.py
+++ temporary directory/not tested.py
@@ -1,1 +1,8 @@
+from pyrogram.raw.functions.channels import GetLeftChannels
 
+#Получите список каналов / супергрупп, которые мы оставили
+@app.on_message()
+async def channels_GetLeftChannels(client, message):
+	msg = await app.send(GetLeftChannels(offset=-1), retries=10, timeout=3.0, sleep_threshold=3.0)
+	await asyncio.sleep(3)
+	print(msg)
