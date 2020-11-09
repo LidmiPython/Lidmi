f83302c46d360676a079ee36eb7b7f443301ec7c
Josesofdess <67865208+Josesofdess@users.noreply.github.com>
2020-11-09 02:10:29 +0200

    Update DeleteHistory.py

--- pyrogram/Raw Functions/Channels/DeleteHistory.py
+++ pyrogram/Raw Functions/Channels/DeleteHistory.py
@@ -4,6 +4,6 @@ from pyrogram.raw.functions.channels import DeleteHistory
 @app.on_message(message_text_filter(f"123123123"))
 async def channels_DeleteChannel(client, message):
 	msg = await app.send(DeleteHistory(
-		channel=await app.resolve_peer(-1001322182991),
+		channel=await app.resolve_peer(-1001158541073),
 		max_id=1))
 	print(msg)
