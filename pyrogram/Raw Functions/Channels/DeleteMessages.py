371f3cc283e69d596e94557c8a3814f872481873
Josesofdess <67865208+Josesofdess@users.noreply.github.com>
2020-11-09 02:14:48 +0200

    Update DeleteMessages.py

--- pyrogram/Raw Functions/Channels/DeleteMessages.py
+++ pyrogram/Raw Functions/Channels/DeleteMessages.py
@@ -1,7 +1,7 @@
 from pyrogram.raw.functions.channels import DeleteMessages
 
 #Удалить сообщения в канале / супергруппе
-@app.on_message(message_text_filter(f"123123123"))
+@app.on_message()
 async def channels_DeleteMessages(client, message):
 	msg = await app.send(DeleteMessages(
 		channel=await app.resolve_peer(-1001158541073),
