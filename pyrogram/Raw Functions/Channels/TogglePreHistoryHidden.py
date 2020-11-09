093df93bc7d1c740cb18e105b3cd8503c27af699
Smoked_sempai <67865208+Josesofdess@users.noreply.github.com>
2020-11-09 07:32:29 +0200

    Update TogglePreHistoryHidden.py

--- pyrogram/Raw Functions/Channels/TogglePreHistoryHidden.py
+++ pyrogram/Raw Functions/Channels/TogglePreHistoryHidden.py
@@ -1,7 +1,7 @@
 from pyrogram.raw.functions.channels import TogglePreHistoryHidden
 
 #Скрыть / показать историю сообщений для новых пользователей канала / супергруппы
-@app.on_message(message_text_filter(f"123123123"))
+@app.on_message()
 async def channels_TogglePreHistoryHidden(client, message):
 	msg = await app.send(TogglePreHistoryHidden(channel=await app.resolve_peer(-1001158541073),
 			enabled=True))
