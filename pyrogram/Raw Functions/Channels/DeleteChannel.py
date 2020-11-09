99f90da9b25cf2b3ed8ea34e06499f5c93584db5
Josesofdess <67865208+Josesofdess@users.noreply.github.com>
2020-11-09 02:00:25 +0200

    Create DeleteChannel.py

---
+++ pyrogram/Raw Functions/Channels/DeleteChannel.py
@@ -0,0 +1,6 @@
+#Удалить канал/супергруппу.
+@app.on_message(message_text_filter(f"123123123"))
+async def channels_DeleteChannel(client, message):
+	msg = await app.send(DeleteChannel(
+		channel=await app.resolve_peer(-1001435500780)))
+	print(msg)
