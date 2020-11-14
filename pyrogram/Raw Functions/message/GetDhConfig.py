2308ff5c7adbca6e4f5304a34262d18a8e88e984
Smoked_sempai <67865208+Josesofdess@users.noreply.github.com>
2020-11-14 09:50:02 +0200

    Update GetDhConfig.py

--- pyrogram/Raw Functions/message/GetDhConfig.py
+++ pyrogram/Raw Functions/message/GetDhConfig.py
@@ -1,6 +1,6 @@
 from pyrogram.raw.functions.messages import GetDhConfig
 #ПВозвращает параметры конфигурации для генерации ключа Диффи-Хеллмана. Может также возвращать случайную последовательность байтов требуемой длины.
-@app.on_message(message_text_filter(f"GetDhConfig"))
+@app.on_message()
 async def messages_GetDhConfig(client, message):
 	msg = await app.send(GetDhConfig(version=0,
 		random_length=0))
