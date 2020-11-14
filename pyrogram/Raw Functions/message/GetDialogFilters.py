62dbdb840c601bd92e58ef41ef2b48b8ab6d2fa7
Smoked_sempai <67865208+Josesofdess@users.noreply.github.com>
2020-11-14 09:49:39 +0200

    Update GetDialogFilters.py

--- pyrogram/Raw Functions/message/GetDialogFilters.py
+++ pyrogram/Raw Functions/message/GetDialogFilters.py
@@ -1,6 +1,6 @@
 from pyrogram.raw.functions.messages import GetDialogFilters
-#Получить папки
-@app.on_message(message_text_filter(f"GetDialogFilters"))
+#Получить папки и список чатов в папке
+@app.on_message()
 async def messages_GetDialogFilters(client, message):
 	msg = await app.send(GetDialogFilters())
 	print(msg)
