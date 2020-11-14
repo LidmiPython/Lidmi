3f3d59bbe0931a8fb42e1cae542be169d28ec859
Smoked_sempai <67865208+Josesofdess@users.noreply.github.com>
2020-11-14 13:04:25 +0200

    Update last_message.py

--- pyrogram/Ready Solutions/last_message.py
+++ pyrogram/Ready Solutions/last_message.py
@@ -2,7 +2,6 @@ from datetime import datetime, timedelta
 
 @app.on_message(filters.me, group=-1)
 async def last_message(client, message):
-	from datetime import datetime, timedelta
 	await app.update_profile(bio=f"[{datetime.strftime(datetime.now(), '%H:%M:%S')}] ⇒ last message")
 '''
 Идея взята у t.me/CorneiZeR
