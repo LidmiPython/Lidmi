c8c31190c180e9245ece6bfba0ec8292cdd7bbef
Smoked_sempai <67865208+Josesofdess@users.noreply.github.com>
2020-11-14 09:48:35 +0200

    Update GetDocumentByHash.py

--- pyrogram/Raw Functions/message/GetDocumentByHash.py
+++ pyrogram/Raw Functions/message/GetDocumentByHash.py
@@ -1,6 +1,6 @@
 from pyrogram.raw.functions.messages import GetDocumentByHash
 #Получить документ по его хешу SHA256, в основном используется для гифок
-@app.on_message(message_text_filter(f"GetDocumentByHash"))
+@app.on_message()
 async def messages_GetDocumentByHash(client, message):
 	msg = await app.send(GetDocumentByHash(Sha256=0,
 		size=0,
