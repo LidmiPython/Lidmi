567dc242b23002669ed440878019da78b48b670e
Smoked_sempai <67865208+Josesofdess@users.noreply.github.com>
2020-11-14 09:49:06 +0200

    Update GetDialogUnreadMarks.py

--- pyrogram/Raw Functions/message/GetDialogUnreadMarks.py
+++ pyrogram/Raw Functions/message/GetDialogUnreadMarks.py
@@ -1,6 +1,6 @@
 from pyrogram.raw.functions.messages import GetDialogUnreadMarks
 #Получить диалоги, помеченные вручную как непрочитанные
-@app.on_message(message_text_filter(f"GetDialogUnreadMarks"))
+@app.on_message()
 async def messages_GetDialogUnreadMarks(client, message):
 	msg = await app.send(GetDialogUnreadMarks())
 	print(msg)
