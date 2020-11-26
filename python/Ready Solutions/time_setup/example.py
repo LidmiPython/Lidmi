7e8519f42b6d71e75033437758ccaab12a468029
Smoked_sempai <67865208+LidmiPython@users.noreply.github.com>
2020-11-26 19:15:19 +0200

    Update example.py

--- python/Ready Solutions/time_setup/example.py
+++ python/Ready Solutions/time_setup/example.py
@@ -1,12 +1,12 @@
 from pyrogram import filters
-from time_setup.SETUP import DATES_SETUP
+from time_setup.SETUP import DATES_SETUP # Импорт класса
 
-bts = DATES_SETUP()
+bts = DATES_SETUP() # Обьявление класса
 
 @app.on_message(filters.me, group=-1)
 def ls(client, message):
-	await app.send_message(message.chat.id ,f"{bts.bot_runned()}")
+	await app.send_message(message.chat.id ,f"{bts.bot_runned()}") # Сколько работает программа
 
 if __name__ == '__main__':
-	bts.reset_date() # Обновление времени
+	bts.reset_date() # Обновление или сборс времени
 	app.run()
